d071ef3fdd5f89ef252bf733de89630fe714c1c8
JustinM12345 <justin.mui25@ycdsbk12.ca>
2025-05-13 13:05:22 -0400

    PowerFunction completed

---
+++ src/recursion/PowerFunction.java
@@ -0,0 +1,16 @@
+package recursion;
+
+public class PowerFunction {
+
+    public static int power(int base, int exponent){
+        if (exponent == 1){
+            return base;
+        }
+        return base * power(base, exponent - 1);
+    }
+
+    public static void main(String[] args){
+        System.out.println(power(3, 4));
+    }
+    
+}
